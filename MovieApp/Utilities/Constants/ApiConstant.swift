d1fbb312b8379cc699ae721880775e8d4ff27c25
Buğra <54468032+Bugrayenidunya@users.noreply.github.com>
2020-10-05 13:45:23 +0300

    Update ApiConstant.swift

--- MovieApp/Utilities/Constants/ApiConstant.swift
+++ MovieApp/Utilities/Constants/ApiConstant.swift
@@ -10,7 +10,7 @@ import Foundation
 extension Constant {
     
     class ApiConstant {
-        static let apiKey = "7764e4f8b3072f2a96414e08c8878490"
+        static let apiKey = "YOUR_API_KEY"
         static let baseURL = "https://api.themoviedb.org/3"
     }
     
